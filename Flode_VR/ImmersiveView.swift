9ab9d2cf6492876c1c868eb6a28b5195f1fc50ec
Tadeo <tadeodonegana@gmail.com>
2024-02-08 16:23:34 -0300

    Replicate url requests

--- Flode_VR/ImmersiveView.swift
+++ Flode_VR/ImmersiveView.swift
@@ -1,10 +1,3 @@
-//
-//  ImmersiveView.swift
-//  Flode_VR
-//
-//  Created by Tadeo Donegana Braunschweig on 06/02/2024.
-//
-
 import SwiftUI
 import RealityKit
 import RealityKitContent
@@ -28,7 +21,12 @@ struct ImmersiveView: View {
             //print("Latest SkyBoxSettings is: \(skyBoxSettings.currentSkybox)")
             
             // Update current skybox
-            updateSkybox(with: skyBoxSettings.currentSkybox, content: content)
+            //Check if the current skybox is a URL or a prebuilt skybox
+            if skyBoxSettings.currentSkybox.contains("http") {
+                updateSkyboxURL(with: skyBoxSettings.currentSkybox, content: content)
+            } else {
+                updateSkybox(with: skyBoxSettings.currentSkybox, content: content)
+            }
         }
         .onAppear(perform: {
             // Present the skybox control window
@@ -42,8 +40,13 @@ struct ImmersiveView: View {
         let skyBoxMesh = MeshResource.generateSphere(radius: 1000)
         // Material (skybox image)
         var skyBoxMaterial = UnlitMaterial()
+        let remoteURL = URL(string: "https://replicate.delivery/pbxt/OtqGeEiTTfk7CU7jj6ogbwRmVs7dxKbaFtax5FmyqNtjKXUSA/6-final.png")!
+        let fileURL = FileManager.default.temporaryDirectory.appendingPathComponent(UUID().uuidString)
+        let data = try! Data(contentsOf: remoteURL)
+        try! data.write(to: fileURL)
+
         // TODO: Ver como se va a poder cargar la textura a partir de una request de API
-        guard let skyBoxTexture = try? TextureResource.load(named: "anime") else {return nil}
+        guard let skyBoxTexture = try? TextureResource.load(contentsOf: fileURL) else {return nil}
         skyBoxMaterial.color = .init(texture: .init(skyBoxTexture))
         // Entity
         let skyBoxEntity = Entity()
@@ -67,9 +70,42 @@ struct ImmersiveView: View {
         let skyBoxEntity = content.entities.first{ entity in
             entity.name == "SkyBox"
         }
-        
+
+
+
         // Update its material (to latest skybox)
         guard let updatedSkyBoxTexture = try? TextureResource.load(named: newSkyBoxName) else {
+            return
+        }
+        
+        var updatedskyBoxMaterial = UnlitMaterial()
+        updatedskyBoxMaterial.color = .init(texture: .init(updatedSkyBoxTexture))
+        
+        skyBoxEntity?.components.set(
+            ModelComponent(
+            mesh: MeshResource.generateSphere(radius: 1000),
+                    materials: [updatedskyBoxMaterial]
+                )
+            )
+        
+    }
+
+    private func updateSkyboxURL (with newSkyBoxName:String, content:RealityViewContent){
+        // Get skybox entity from content
+        // Loop trought the entities and retrieve the one that has SkyBox name
+        let skyBoxEntity = content.entities.first{ entity in
+            entity.name == "SkyBox"
+        }
+        
+        let remoteURL = URL(string:newSkyBoxName )!
+        let fileURL = FileManager.default.temporaryDirectory.appendingPathComponent(UUID().uuidString)
+        let data = try! Data(contentsOf: remoteURL)
+        try! data.write(to: fileURL)
+
+
+        // Update its material (to latest skybox)
+        guard let updatedSkyBoxTexture = try? TextureResource.load(contentsOf: fileURL)
+        else {
             return
         }
         
--- Flode_VR/SkyBoxControlsView.swift
+++ Flode_VR/SkyBoxControlsView.swift
@@ -4,47 +4,119 @@
 //
 //  Created by Tadeo Donegana Braunschweig on 06/02/2024.
 //
-
 import SwiftUI
+import Replicate
 
 struct SkyBoxControlsView: View {
+    @EnvironmentObject var skyBoxSettings: SkyboxSettings
+    @State private var inputUrl: String = ""
+
+    var body: some View {
+        VStack {
+            HStack {
+                SkyBoxButton(onClick: {
+                    skyBoxSettings.currentSkybox = "anime"
+                }, iconName: "tree")
+                
+                SkyBoxButton(onClick: {
+                    skyBoxSettings.currentSkybox = "beach"
+                }, iconName: "moon")
+                
+                SkyBoxButton(onClick: {
+                    skyBoxSettings.currentSkybox = "stadium"
+                }, iconName: "sunset")
+            }
+            // New UI elements for URL input and send button
+            HStack {
+                TextField("Enter SkyBox URL", text: $inputUrl)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                
+                Button(action: {
+                    // Call API and update SkyBoxSettings with the response
+                    callApiAndUpdateSkybox(with: inputUrl)
+                }) {
+                    Image(systemName: "wand.and.stars.inverse") // Adjust the icon to match your UI
+                        .padding()
+                }
+            }
+        }
+    }
     
-    // To have access to the object passed to the view
-    @EnvironmentObject var skyBoxSettings:SkyboxSettings
+    struct PredictionInput: Codable {
+        var version: String
+        var input: Prompt
+    }
+
+    struct Prompt: Codable {
+        var prompt: String
+    }
+    
+    // Llama a la API y devuelve la URL de la imagen generada
+    func callApiAndUpdateSkybox(with url: String) {
+        self.skyBoxSettings.currentSkybox = "https://replicate.delivery/pbxt/OtqGeEiTTfk7CU7jj6ogbwRmVs7dxKbaFtax5FmyqNtjKXUSA/6-final.png"
+        
+        
+        guard let url = URL(string: "https://api.replicate.com/v1/predictions") else { return }
+        
+        let predictionInput = PredictionInput(version: "76acc4075d0633dcb3823c1fed0419de21d42001b65c816c7b5b9beff30ec8cd", input: Prompt(prompt: "hdri view, aurora night in a mountain"))
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("Token $REPLICATE_API_TOKEN", forHTTPHeaderField: "Authorization") // Reemplaza $REPLICATE_API_TOKEN con tu token real
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        do {
+            let jsonData = try JSONEncoder().encode(predictionInput)
+            request.httpBody = jsonData
+        } catch {
+            print("Error encoding input: \(error)")
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print("Request error: \(error)")
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                print(response as? HTTPURLResponse as Any)
+                print("Invalid response or status code")
+                return
+            }
+            
+            guard let data = data else {
+                print("No data received")
+                return
+            }
+            
+            // Intenta imprimir la respuesta como un String
+            if let responseString = String(data: data, encoding: .utf8) {
+                print("Response: \(responseString)")
+            } else {
+                print("Error converting data to String")
+            }
+        }.resume()
+    }
+    
+}
+
+struct SkyBoxButton: View {
+    var onClick: () -> Void
+    var iconName: String
     
     var body: some View {
-        HStack {
-            SkyBoxButton(onClick: {
-                // Change skybox
-                skyBoxSettings.currentSkybox = "anime"
-            }, iconName: "tree")
-            
-            SkyBoxButton(onClick: {
-                skyBoxSettings.currentSkybox = "beach"
-            }, iconName: "moon")
-            
-            SkyBoxButton(onClick: {
-                skyBoxSettings.currentSkybox = "stadium"
-            }, iconName: "sunset")
-        }
+        Button(action: onClick, label: {
+            Image(systemName: iconName)
+        })
     }
 }
 
-#Preview {
-    SkyBoxControlsView()
-}
 
-// Button view
-struct SkyBoxButton: View {
-    var onClick: () -> Void
-    var iconName:String
-    
-    var body: some View {
-        Button(action: {
-            // Change skybox
-            onClick()
-        }, label: {
-            Image(systemName: iconName)
-        })
+// Example Preview Provider
+struct SkyBoxControlsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SkyBoxControlsView().environmentObject(SkyboxSettings())
     }
 }
